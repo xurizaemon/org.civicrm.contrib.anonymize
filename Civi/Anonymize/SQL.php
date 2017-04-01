fdbb31dfea597a90f54397abc44a1775afcf04ef
Sean Madsen <sean@seanmadsen.com>
2017-04-01 10:59:04 -0600

    Library to produce SQL which generates random stuff

---
+++ Civi/Anonymize/SQL.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace Civi\Anonymize;
+
+use \DateTime;
+
+/**
+ * A collection of functions which produce SQL code to do things like generate
+ * random values
+ *
+ * @package Civi\Anonymize
+ */
+class SQL {
+
+  /**
+   * Returns an SQL expression that randomly chooses between a supplied value or
+   * a NULL value.
+   *
+   * @param $value string An SQL expression to use as the resulting value
+   * when we don't choose NULL.
+   *
+   * @param $nullLikelihood float A number greater than 0 and less than 1
+   * which represents the likelihood of choosing NULL over $value.
+   * When $nullLikelihood = 0.0001 then NULL will almost never be chosen.
+   * When $nullLikelihood = 0.9999 then NULL will almost always be chosen.
+   *
+   * @return string
+   *
+   * @throws \Exception when $nullLikelihood is out of bounds
+   */
+  public static function valueOrNull($value, $nullLikelihood) {
+    $p = (float) $nullLikelihood;
+    $pIsInRange = (bool) ($p > 0 && $p < 1);
+    if (!$pIsInRange) {
+      throw new \Exception("nullLikelihood must be greater than zero "
+        . "and less than 1. Value given: " . $nullLikelihood);
+    }
+    return "IF(rand() <= $p, $value, NULL)";
+  }
+
+  /**
+   * Returns an SQL expression that generates a random date between within the
+   * specified bounds
+   *
+   * Dates can be specified as either a string like '2017-01-01' or 'now' or by
+   * passing a PHP DateTime object
+   *
+   * @param $start string|DateTime The lower bound for dates to be generated
+   * @param $end string|DateTime the upper bound for dates to be generated
+   * @return string
+   */
+  public static function randomDate($start, $end) {
+    if (!is_a($start, "DateTime")) {
+      $start = new DateTime($start);
+    }
+    if (!is_a($end, "DateTime")) {
+      $end = new DateTime($end);
+    }
+    $daysMax = $end->diff($start)->days;
+    $days = self::randomInteger(0, $daysMax);
+    $startDateString = '"' . $start->format('Y-m-d') . '"';
+    return "$startDateString + INTERVAL $days DAY";
+  }
+
+  /**
+   * Returns an SQL expression to generate one random integer within a specified
+   * range
+   *
+   * @param $min int The lower bound for the number to be generated
+   * @param $max int The upper bound for the number to be generated
+   * @return string
+   */
+  public static function randomInteger($min, $max) {
+    return "FLOOR($min + RAND() * $max + 1)";
+  }
+
+  /**
+   * Returns an SQL expression to generate one random character of the specified
+   * type
+   *
+   * @param $type string must be 'lower', 'upper', or 'digit'
+   * @return string
+   * @throws \Exception when type is invalid
+   */
+  public static function randomChar($type) {
+    $types = array(
+      'lower' => array('a', 'z'),
+      'upper' => array('A', 'Z'),
+      'digit' => array('0', '9'),
+    );
+    if (!array_key_exists($type, $types)) {
+      throw new \Exception("Invalid character type '{$type}'. "
+        . "Type must be one of: " . implode(', ', array_keys($types)));
+    }
+    $charCodeMin = ord($types[$type][0]);
+    $charCodeMax = ord($types[$type][1]);
+    $charCode = self::randomInteger($charCodeMin, $charCodeMax);
+    return "CHAR($charCode)";
+  }
+
+  /**
+   * Returns SQL that produces a random string of characters.
+   *
+   * @param $case string What should the case of the returned string be?
+   * Options are: 'lower', 'upper', or 'sentence'
+   *
+   * @param $length int The length of string to return
+   *
+   * @return string
+   *
+   * @throws \Exception if case is invalid
+   */
+  private static function fixedLengthRandomString($case, $length) {
+    $cases = array('lower', 'upper', 'sentence');
+    if (!in_array($case, $cases)) {
+      throw new \Exception("Invalid string case $case."
+        . "Options are: " . implode(', ', $cases));
+    }
+    $firstChar = self::randomChar(($case == 'lower') ? 'lower' : 'upper');
+    $otherChars = self::randomChar(($case == 'upper') ? 'upper' : 'lower');
+    $allChars = array_merge(
+      array($firstChar),
+      array_fill(0, max($length) - 1, $otherChars)
+    );
+    return "CONCAT(" . implode(",\n    ", $allChars) . ")";
+  }
+
+  /**
+   * Returns SQL that produces a random string of characters.
+   *
+   * @param $case string What should the case of the returned string be?
+   * Options are: 'lower' (default), 'upper', or 'sentence'
+   *
+   * @param $length int|array The minimum and maximum length of the string to
+   * be generated
+   *
+   * @return string
+   */
+  public static function randomString($case = 'lower', $length = 10) {
+    if (!is_array($length)) {
+      $length = array($length, $length);
+    }
+    // Generate a string as long as we might need
+    $longString = self::fixedLengthRandomString($case, max($length));
+    if (min($length) == max($length)) {
+      return $longString;
+    }
+    else {
+      $stringLength = self::randomInteger(min($length), max($length));
+      // Take only a part of the string, beginning with the first character
+      // and taking a random length
+      return "SUBSTRING($longString, 1, $stringLength)";
+    }
+  }
+
+}
