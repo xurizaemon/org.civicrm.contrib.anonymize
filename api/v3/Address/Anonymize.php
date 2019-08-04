ead8988b8760fd20fd688965a62f10df2576c614
Chris Burgess <chrisburgess@catalyst.net.nz>
2019-08-04 15:49:51 +1200

    🐞 Some addresses weren't stored or anonymised
    Ensures that country data is randomized if existing values present. Also
    ensures street_name and province are updated as required.

--- api/v3/Address/Anonymize.php
+++ api/v3/Address/Anonymize.php
@@ -51,11 +51,40 @@ function civicrm_api3_address_anonymize($params) {
     $values = reset($address['values']);
     $create_params = array(
       'street_number' => $faker->buildingNumber(),
-      'street_name' => $faker->streetName(),
+      // Faker streetName appends streetSuffix anyway.
+      'street_name' => $faker->lastName(),
       'street_type' => $faker->streetSuffix(),
       'city' => $faker->city(),
       'postal_code' => $faker->postCode(),
+      'postal_code_suffix' => empty($values['postal_code_suffix']) ? '' : $faker->postCode(),
     );
+    // Put full street_address in as well as street_name, street_suffix etc.
+    // If we don't do this, confidential data may leak via those fields.
+    $create_params['street_address'] = "{$create_params['street_number']} {$create_params['street_name']} {$create_params['street_type']}";
+
+    // Trim street_ty[e to 8 chars for CiviCRM storage.
+    if (strlen($create_params['street_type']) > 8) {
+      $create_params['street_type'] = '';
+    }
+
+    // If country_id is populated, replace with a random country_id.
+    // We should then select a random province from that country.
+    if (!empty($values['country_id'])) {
+      $countries = civicrm_api3('Country', 'get', [
+        'sequential' => 1,
+        'options' => ['limit' => 0],
+      ]);
+      $create_params['country_id'] = $countries['values'][array_rand($countries['values'])]['id'];
+      if (!empty($values['state_province_id'])) {
+        $provinces = civicrm_api3('StateProvince', 'get', [
+          'sequential' => 1,
+          'country_id' => $create_params['country_id'],
+          'options' => ['limit' => 0],
+        ]);
+        $create_params['state_province_id'] = $provinces['values'][array_rand($provinces['values'])]['id'];
+      }
+    }
+
     $address = civicrm_api3('Address', 'Create', array_merge($get_params, $create_params));
     return civicrm_api3_create_success($address, $get_params, 'Contact', 'Get');
   }
